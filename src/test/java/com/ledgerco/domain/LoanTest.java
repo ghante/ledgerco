31c2276b22c155526542af1dbc01c3fe76144922
Anirudha Ghante <ghante@thoughtworks.com>
2022-01-08 16:04:16 +0530

    adding test for multiple payments

--- src/test/java/com/ledgerco/domain/LoanTest.java
+++ src/test/java/com/ledgerco/domain/LoanTest.java
@@ -134,4 +134,27 @@ class LoanTest {
         assertThat(loan.amountPaidAfterInstallments(6)).isEqualTo(2020);
         assertThat(loan.amountPaidAfterInstallments(7)).isEqualTo(2020);
     }
+
+    @Test
+    void shouldCalculateCorrectAmountAndInstallmentsForMultipleLumpSumPayments() {
+        Loan loan = new Loan("bankName", "borrowerName",
+                2000, 0.5, 2);
+
+        loan.addPayment(0, 500);
+        loan.addPayment(2, 400);
+
+        assertThat(loan.amountPaidAfterInstallments(0)).isEqualTo(0);
+        assertThat(loan.amountPaidAfterInstallments(1)).isEqualTo(337 + 500);
+        assertThat(loan.amountPaidAfterInstallments(2)).isEqualTo(337 * 2 + 500);
+        assertThat(loan.amountPaidAfterInstallments(3)).isEqualTo(337 * 3 + 500 + 400);
+        assertThat(loan.amountPaidAfterInstallments(4)).isEqualTo(2020);
+        assertThat(loan.amountPaidAfterInstallments(5)).isEqualTo(2020);
+
+        assertThat(loan.remainingInstallmentsAfter(0)).isEqualTo(6);
+        assertThat(loan.remainingInstallmentsAfter(1)).isEqualTo(4);
+        assertThat(loan.remainingInstallmentsAfter(2)).isEqualTo(3);
+        assertThat(loan.remainingInstallmentsAfter(3)).isEqualTo(1);
+        assertThat(loan.remainingInstallmentsAfter(4)).isEqualTo(0);
+        assertThat(loan.remainingInstallmentsAfter(5)).isEqualTo(0);
+    }
 }
